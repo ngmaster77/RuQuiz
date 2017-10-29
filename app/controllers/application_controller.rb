7e59baf5e22e5f9d45c0af4354260a0f7ea5a4ab
Nestor <alu0100812503@ull.edu.es>
2017-10-29 10:33:40 +0000

    Primeros pasos..

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+class ApplicationController < Sinatra::Base
+  
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do 
+    erb :home
+  end
+
+end
+
+
+  
