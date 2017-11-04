75e4793847d219b6307038a72253a3bfa5edebbf
Nestor <alu0100812503@ull.edu.es>
2017-11-04 22:53:58 +0000

    Modificado esquema BBDD: ahora se encriptan las contraseñas

--- db/migrate/20171104223527_create_users.rb
+++ db/migrate/20171104223527_create_users.rb
@@ -1,9 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    drop_table :users
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password
+      t.string :password_digest
+      t.boolean :instructor
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104221010) do
+ActiveRecord::Schema.define(version: 20171104223527) do
 
   create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
     t.string   "value"
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(version: 20171104221010) do
   add_index "ar_internal_metadata", ["key"], name: "sqlite_autoindex_ar_internal_metadata_1", unique: true
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
+    t.string  "name"
+    t.string  "email"
+    t.string  "password_digest"
+    t.boolean "instructor"
   end
 
 end
