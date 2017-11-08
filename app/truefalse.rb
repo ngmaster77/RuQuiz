b6644a7753665051ef93f895e338afb7f0a36d43
Carlos de Armas <alu0100816167@ull.edu.es>
2017-11-08 23:35:59 +0000

    Haciendo atributos accesibles

--- app/truefalse.rb
+++ app/truefalse.rb
@@ -1,14 +1,14 @@
 class TrueFalse < Pregunta
+  attr_accessor :text, :answer, :puntuation
   def initialize(enunciado, respuesta, puntuacion)
     @text = enunciado
     @answer = respuesta
     @puntuation = puntuacion
   end
-  
+
   def toRUQL
-    str = "truefalse "
+    str = 'truefalse '
     str << "'#{@text}', #{@answer}"
     str
   end
 end
-
