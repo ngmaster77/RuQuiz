a6b2be74bd65a95602161c3e8e3b99f2fb5c9397
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-11-09 00:35:29 +0000

    Completando pregunta verdadero/falso

--- public/js/addQuestion.js
+++ public/js/addQuestion.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
     var pregunta = 1;
     $("#add1").click(function() {        
         var questionTitle = "<hr><h3>Pregunta " + pregunta + " <small>(Verdadero o Falso)</small></h3>";
-        pregunta++;
-        var inputQuestion = "<div class=\"form-row\"><div class=\"form-group col-md-8\"><label for=\"inputQuestionTrueFalse" + pregunta + "\">Enunciado</label><input type=\"text\" class=\"form-control\" id=\"inputQuestionTrueFalse" + pregunta + "\"></div>";
-        var inputAnswer = "<div class=\"form-group col-md-4\"><label for=\"inputAnswerTrueFalse" + pregunta + "\">Respuesta</label><select id=\"inputAnswerTrueFalse" + pregunta + "\" class=\"form-control\"><option selected disabled>Seleccionar respuesta...</option><option>Verdadero</option><option>Falso</option></select></div></div>";
-
+        var inputQuestion = "<div class=\"form-row\"><div class=\"form-group col-md-8\"><label for=\"inputQuestionTrueFalse" + pregunta + "\">Enunciado</label><input type=\"text\" class=\"form-control\" id=\"inputQuestionTrueFalse" + pregunta + "\" name=\"inputQuestionTrueFalse" + pregunta + "\"></div>";
+        var inputAnswer = "<div class=\"form-group col-md-4\"><label for=\"inputAnswerTrueFalse" + pregunta + "\">Respuesta</label><select id=\"inputAnswerTrueFalse" + pregunta + "\" class=\"form-control\" name=\"inputAnswerTrueFalse" + pregunta + "\"><option selected disabled>Seleccionar respuesta...</option><option value=\"true\">Verdadero</option><option value=\"false\">Falso</option></select></div></div>";
         var question = questionTitle + inputQuestion + inputAnswer;
+        pregunta++;
         $(".questions").append(question);
     });
     $("#add2").click(function() {
