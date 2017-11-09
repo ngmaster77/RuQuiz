9a9b0f17bd70690d8f90a4dbb0e611c3775a0a83
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-11-09 00:03:00 +0000

    Limpiando addQuestion.js

--- public/js/addQuestion.js
+++ public/js/addQuestion.js
@@ -1,20 +1,5 @@
 $(document).ready(function() {
-    console.log("jquery ok")
     var pregunta = 1;
-    $("#add").click(function() {
-        var intId = $("#buildyourform div").length + 1;
-        var fieldWrapper = $("<div class=\"fieldwrapper\" id=\"field" + intId + "\"/>");
-        var fName = $("<input type=\"text\" class=\"fieldname\" />");
-        var fType = $("<select class=\"fieldtype\"><option value=\"checkbox\">Checked</option><option value=\"textbox\">Text</option><option value=\"textarea\">Paragraph</option></select>");
-        var removeButton = $("<input type=\"button\" class=\"remove\" value=\"-\" />");
-        removeButton.click(function() {
-            $(this).parent().remove();
-        });
-        fieldWrapper.append(fName);
-        fieldWrapper.append(fType);
-        fieldWrapper.append(removeButton);
-        $("#buildyourform").append(fieldWrapper);
-    });
     $("#add1").click(function() {        
         var questionTitle = "<hr><h3>Pregunta " + pregunta + " <small>(Verdadero o Falso)</small></h3>";
         pregunta++;
