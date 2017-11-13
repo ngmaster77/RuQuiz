4ad6ef211214e24eb096ae4143357967bfb7f10f
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-11-13 14:39:51 +0000

    Obligando a añadir titulo y descripcion para crear nuevo cuestionario

--- public/js/addQuestion.js
+++ public/js/addQuestion.js
@@ -39,3 +39,18 @@ $(document).ready(function() {
         $(".questions").empty();
     });
 });
+
+(function () {
+    'use strict';
+
+    window.addEventListener('load', function () {
+      var form = document.getElementById('needs-validation');
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    }, false);
+  })();
