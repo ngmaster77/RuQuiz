44beaa7c7356746a357cf7438d74fa30edbcbc8a
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-11-16 23:06:22 +0000

    Arreglando fallo que permitía volver a crear cuestionario sin preguntas

--- public/js/addQuestion.js
+++ public/js/addQuestion.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         var inputQuestion = "<div class=\"form-row\"><div class=\"form-group col-md-8\"><label for=\"inputQuestionTrueFalse" + pregunta + "\">Enunciado</label><input type=\"text\" class=\"form-control\" id=\"inputQuestionTrueFalse" + pregunta + "\" name=\"inputQuestionTrueFalse" + pregunta + "\" required><small class=\"form-text text-muted\">Introduce el enunciado de la pregunta y luego elige si es verdadera o falsa.</small></div>";
         var inputAnswer = "<div class=\"form-group col-md-4\"><label for=\"inputAnswerTrueFalse" + pregunta + "\">Respuesta Correcta</label><select id=\"inputAnswerTrueFalse" + pregunta + "\" class=\"form-control\" name=\"inputAnswerTrueFalse" + pregunta + "\" required><option value=\"\">Seleccionar respuesta...</option><option value=\"true\">Verdadero</option><option value=\"false\">Falso</option></select></div></div>";
         var question = questionTitle + questionType + inputQuestion + inputAnswer;
-        var fieldWrapper = $("<div id=\"question" + pregunta + "\"</div>");
+        var fieldWrapper = $("<div class=\"questionWrapper\" id=\"question" + pregunta + "\"</div>");
         pregunta++;
         fieldWrapper.append(question);
         $(".questions").append(fieldWrapper);
@@ -21,7 +21,7 @@ $(document).ready(function() {
         var inputQuestion = "<div class=\"form-group\"><label for=\"inputQuestionFillIn" + pregunta + "\">Enunciado</label><input type=\"text\" class=\"form-control\" id=\"inputQuestionFillIn" + pregunta + "\" name=\"inputQuestionFillIn" + pregunta + "\" placeholder=\"La capital de EEUU es - y se encuentra en la costa -\" required><small class=\"form-text text-muted\">Escribe un guión (-) donde quieras añadir el espacio en blanco para que el alumno conteste.</small></div>";
         var inputAnswer = "<div class=\"form-group\"><label for=\"inputAnswerTFillIn" + pregunta + "\">Respuesta Correcta</label><input type=\"text\" class=\"form-control\" id=\"inputAnswerFillIn" + pregunta + "\" name=\"inputAnswerFillIn" + pregunta + "\" placeholder=\"Washington,este\" required><small class=\"form-text text-muted\">Escribe las respuestas correctas separadas por coma sin espacios.</small></div>";
         var question = questionTitle + questionType + inputQuestion + inputAnswer;
-        var fieldWrapper = $("<div id=\"question" + pregunta + "\"</div>");
+        var fieldWrapper = $("<div class=\"questionWrapper\" id=\"question" + pregunta + "\"</div>");
         pregunta++;
         fieldWrapper.append(question);
         $(".questions").append(fieldWrapper);
@@ -35,7 +35,7 @@ $(document).ready(function() {
         var inputTrueAnswer = "<div class=\"form-group\"><label for=\"inputTrueAnswerChoice" + pregunta + "\">Respuesta Correcta</label><input type=\"text\" class=\"form-control\" id=\"inputTrueAnswerChoice" + pregunta + "\" name=\"inputTrueAnswerChoice" + pregunta + "\" required><small class=\"form-text text-muted\">Introduce solo una respuesta correcta.</small></div>";
         var inputFalseAnswer = "<div class=\"form-group\"><label for=\"inputFalseAnswerChoice" + pregunta + "\">Respuestas Incorrectas</label><input type=\"text\" class=\"form-control\" id=\"inputFalseAnswerChoice" + pregunta + "\" name=\"inputFalseAnswerChoice" + pregunta + "\" required><small class=\"form-text text-muted\">Escribe las respuestas incorrectas separadas por coma.</small></div>";
         var question = questionTitle + questionType + inputQuestion + inputTrueAnswer + inputFalseAnswer;
-        var fieldWrapper = $("<div id=\"question" + pregunta + "\"</div>");
+        var fieldWrapper = $("<div class=\"questionWrapper\"id=\"question" + pregunta + "\"</div>");
         pregunta++;
         fieldWrapper.append(question);
         $(".questions").append(fieldWrapper);
@@ -49,8 +49,7 @@ $(document).ready(function() {
 
     /* Impedir que se pueda enviar el formulario sin preguntas */
     $('form').submit(function () {
-        if ($('#questionsContainer').is(':empty')){
-            console.log("no hay preguntas");
+        if ($('#questionsContainer').find($(".questionWrapper")).length == 0){
             var alertText = "Debe añadir <strong>al menos 1 pregunta</strong> para crear el cuestionario.";
             var alert = "<br><div class=\"alert alert-danger alert-dismissible fade show\" role=\"alert\">" + alertText + "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button></div>";
             $("#alertEmptyQuestions").append(alert);
@@ -66,6 +65,7 @@ $(document).ready(function() {
 $(document).on('click', '.deleteButton', function(){ 
     var questionNumber = $(this)[0].id.split("-")[1];
     var deleteQuestion = "<hr><div class=\"card border-warning\"><div class=\"card-body text-warning\"><h4 class=\"card-title\">Pregunta " + questionNumber + ": Eliminada</h4><p class=\"card-text\">Esta pregunta ha sido eliminada y no será añadida en el cuestionario.</p></div></div>";
+    $(this).parent().parent().removeClass("questionWrapper")
     $(this).parent().parent().empty();
     $("#question" + questionNumber).append(deleteQuestion);
 });
---
+++ var/8.rb
@@ -0,0 +1,4 @@
+quiz 'asdasdasdasd' do
+fill_in do text 'asdaksd -' 
+answer 'asdasd' end
+end
