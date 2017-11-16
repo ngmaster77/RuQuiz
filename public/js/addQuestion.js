6e5ffd41eea7ec836d1ccfba2c1d82bb9e2d16d2
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-11-16 22:37:16 +0000

    Permitiendo borrar preguntas individualmente al crear cuestionario 🤪

--- public/js/addQuestion.js
+++ public/js/addQuestion.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
     /* Añadir pregunta de tipo: Verdadero o Falso */
     $("#add1").click(function() {        
-        var questionTitle = "<hr><h3>Pregunta " + pregunta + " <small class=\"text-muted\">(Verdadero o Falso)</small></h3>";
+        var questionTitle = "<hr><h3>Pregunta " + pregunta + " <small class=\"text-muted\">(Verdadero o Falso)</small> <button id=\"deleteQuestion-" + pregunta + "\" type=\"button\" class=\"btn btn-danger btn-sm deleteButton\"><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></button></h3>";
         var questionType = "<div class=\"form-group\"><input type=\"text\" class=\"form-control\" name=\"inputQuestionType" + pregunta + "\" value=\"1\" hidden></div>";
         var inputQuestion = "<div class=\"form-row\"><div class=\"form-group col-md-8\"><label for=\"inputQuestionTrueFalse" + pregunta + "\">Enunciado</label><input type=\"text\" class=\"form-control\" id=\"inputQuestionTrueFalse" + pregunta + "\" name=\"inputQuestionTrueFalse" + pregunta + "\" required><small class=\"form-text text-muted\">Introduce el enunciado de la pregunta y luego elige si es verdadera o falsa.</small></div>";
         var inputAnswer = "<div class=\"form-group col-md-4\"><label for=\"inputAnswerTrueFalse" + pregunta + "\">Respuesta Correcta</label><select id=\"inputAnswerTrueFalse" + pregunta + "\" class=\"form-control\" name=\"inputAnswerTrueFalse" + pregunta + "\" required><option value=\"\">Seleccionar respuesta...</option><option value=\"true\">Verdadero</option><option value=\"false\">Falso</option></select></div></div>";
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
     /* Añadir pregunta de tipo: Completar */
     $("#add2").click(function() {
-        var questionTitle = "<hr><h3>Pregunta " + pregunta + " <small class=\"text-muted\">(Completar)</small></h3>";
+        var questionTitle = "<hr><h3>Pregunta " + pregunta + " <small class=\"text-muted\">(Completar)</small> <button id=\"deleteQuestion-" + pregunta + "\" type=\"button\" class=\"btn btn-danger btn-sm deleteButton\"><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></button></h3>";
         var questionType = "<div class=\"form-group\"><input type=\"text\" class=\"form-control\" name=\"inputQuestionType" + pregunta + "\" value=\"2\" hidden></div>";
         var inputQuestion = "<div class=\"form-group\"><label for=\"inputQuestionFillIn" + pregunta + "\">Enunciado</label><input type=\"text\" class=\"form-control\" id=\"inputQuestionFillIn" + pregunta + "\" name=\"inputQuestionFillIn" + pregunta + "\" placeholder=\"La capital de EEUU es - y se encuentra en la costa -\" required><small class=\"form-text text-muted\">Escribe un guión (-) donde quieras añadir el espacio en blanco para que el alumno conteste.</small></div>";
         var inputAnswer = "<div class=\"form-group\"><label for=\"inputAnswerTFillIn" + pregunta + "\">Respuesta Correcta</label><input type=\"text\" class=\"form-control\" id=\"inputAnswerFillIn" + pregunta + "\" name=\"inputAnswerFillIn" + pregunta + "\" placeholder=\"Washington,este\" required><small class=\"form-text text-muted\">Escribe las respuestas correctas separadas por coma sin espacios.</small></div>";
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
     /* Añadir pregunta de tipo: Multirespuesta */
     $("#add3").click(function() {
-        var questionTitle = "<hr><h3>Pregunta " + pregunta + " <small class=\"text-muted\">(Multirespuesta)</small></h3>";
+        var questionTitle = "<hr><h3>Pregunta " + pregunta + " <small class=\"text-muted\">(Multirespuesta)</small> <button id=\"deleteQuestion-" + pregunta + "\" type=\"button\" class=\"btn btn-danger btn-sm deleteButton\"><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></button></h3>";
         var questionType = "<div class=\"form-group\"><input type=\"text\" class=\"form-control\" name=\"inputQuestionType" + pregunta + "\" value=\"3\" hidden></div>";
         var inputQuestion = "<div class=\"form-group\"><label for=\"inputQuestionChoice" + pregunta + "\">Enunciado</label><input type=\"text\" class=\"form-control\" id=\"inputQuestionChoice" + pregunta + "\" name=\"inputQuestionChoice" + pregunta + "\" required><small class=\"form-text text-muted\">Introduce el enunciado de la pregunta.</small></div>";
         var inputTrueAnswer = "<div class=\"form-group\"><label for=\"inputTrueAnswerChoice" + pregunta + "\">Respuesta Correcta</label><input type=\"text\" class=\"form-control\" id=\"inputTrueAnswerChoice" + pregunta + "\" name=\"inputTrueAnswerChoice" + pregunta + "\" required><small class=\"form-text text-muted\">Introduce solo una respuesta correcta.</small></div>";
@@ -60,6 +60,14 @@ $(document).ready(function() {
 
     /* Permitir cerrar una alerta */
     $(".alert").alert()
+});
+
+/* Borrar pregunta de manera individual */
+$(document).on('click', '.deleteButton', function(){ 
+    var questionNumber = $(this)[0].id.split("-")[1];
+    var deleteQuestion = "<hr><div class=\"card border-warning\"><div class=\"card-body text-warning\"><h4 class=\"card-title\">Pregunta " + questionNumber + ": Eliminada</h4><p class=\"card-text\">Esta pregunta ha sido eliminada y no será añadida en el cuestionario.</p></div></div>";
+    $(this).parent().parent().empty();
+    $("#question" + questionNumber).append(deleteQuestion);
 });
 
 /* Función que comprueba que el cuestionario tiene título y descripción */
---
+++ var/6.rb
@@ -0,0 +1,2 @@
+quiz 'aaaa' do
+end
