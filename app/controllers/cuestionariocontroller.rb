8f82aaa5b7b69f7be6ecea5f20d7956b0e324d39
Carlos de Armas <alu0100816167@ull.edu.es>
2017-11-17 09:57:15 +0000

    Añadiendo flash para mensajes en el controlador

--- app/controllers/cuestionariocontroller.rb
+++ app/controllers/cuestionariocontroller.rb
@@ -1,5 +1,4 @@
 class CuestionarioController < ApplicationController
-
   get '/newcuestionario' do
     @user = User.find(session[:id])
     erb :newcuestionario
@@ -11,35 +10,32 @@ class CuestionarioController < ApplicationController
     @data = params.values.to_a
     iterator = 0
     while iterator < @data.length
-         if @data[iterator] == '1'
-           @verdadero = PreguntaVerdadero.new(@data[iterator+1],@data[iterator+2])
-           array << @verdadero
-         elsif  @data[iterator] == '2'
-           @completar = PreguntaRelleno.new(@data[iterator+1],@data[iterator+2])
-           array << @completar
-         elsif @data[iterator] == '3'
-           @seleccion = PreguntaSeleccion.new(@data[iterator+1],@data[iterator+2],@data[iterator+3])
-           array << @seleccion
-         end
-         iterator += 1
+      if @data[iterator] == '1'
+        @verdadero = PreguntaVerdadero.new(@data[iterator + 1], @data[iterator + 2])
+        array << @verdadero
+      elsif @data[iterator] == '2'
+        @completar = PreguntaRelleno.new(@data[iterator + 1], @data[iterator + 2])
+        array << @completar
+      elsif @data[iterator] == '3'
+        @seleccion = PreguntaSeleccion.new(@data[iterator + 1], @data[iterator + 2], @data[iterator + 3])
+        array << @seleccion
+      end
+      iterator += 1
     end
     @questionario = Questionario.new(@data[0], array)
-    @cuestionario = Cuestionario.new(titulo:@data[0],descripcion:@data[1],creador:@user.name,npreguntas: array.length, notaaprobar: ((array.length)/2), notamaxima: array.length)
+    @cuestionario = Cuestionario.new(titulo: @data[0], descripcion: @data[1], creador: @user.name, npreguntas: array.length, notaaprobar: (array.length / 2), notamaxima: array.length)
     if @cuestionario.save
-          $identificador = @cuestionario.id
-          @questionario.write($identificador)
-          flash[:error] = "Creado con éxito!"
-          redirect to '/home_profesor'
+      $identificador = @cuestionario.id
+      @questionario.write($identificador)
+
+      redirect to '/home_profesor', flash[:notice] = 'Cuestionario creado con éxito!'
     else
-          flash[:error] = "Error al crear el cuestionario"
-          redirect to '/newcuestionario'
+      redirect to '/newcuestionario', flash[:error] = 'Error al crear el cuestionario'
     end
   end
-
 
   get '/showcuestionarios' do
     @cuestionarios = Cuestionario.all
     erb :showcuestionarios
   end
-
 end
