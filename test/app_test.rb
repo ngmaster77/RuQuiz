5f1c05ec146c2197a3bc45d9d51c47ba1ec075dc
Carlos de Armas <alu0100816167@ull.edu.es>
2017-11-18 14:31:56 +0000

    los ficheros de la aplicación cargan correctamente

--- test/app_test.rb
+++ test/app_test.rb
@@ -7,34 +7,3 @@ require 'capybara'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
-Capybara.app = ApplicationController
-# Capybara.app_host = 'http://localhost:9292/'
-
-describe '# Prueba de rutas', type: :feature do
-  it 'Carga la página principal' do
-    visit '/'
-    expect(200).to eq(page.status_code)
-  end
-  
-  it 'Carga la página de login' do
-  visit '/login'
-  expect(200).to eq(page.status_code)
-  end
-  
-  it "Carga la página de registro" do
-    visit '/registro'
-    expect(200).to eq(page.status_code)
-  end
-end
-
-
-describe '# Prueba inicio de sesión/registro', type: :feature do
-  it 'Login con usuario profesor' do
-    visit '/login'
-    fill_in 'name', with: 'profesor'
-    fill_in 'password', with: '12345'
-    click_button 'Log in'
-    expect(200).to eq(page.status_code)
-    expect(current_path).to eql('/home_profesor')
-  end
-end
