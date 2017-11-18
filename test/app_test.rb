77c5db478f7665b9c0afb3f499776845f6ce9f37
Carlos de Armas <alu0100816167@ull.edu.es>
2017-11-18 14:32:41 +0000

    Añadiendo prueba de rutas principales

--- test/app_test.rb
+++ test/app_test.rb
@@ -7,3 +7,22 @@ require 'capybara'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
+Capybara.app = ApplicationController
+
+
+describe '# Prueba de rutas', type: :feature do
+  it 'Carga la página principal' do
+    visit '/'
+    expect(200).to eq(page.status_code)
+  end
+  
+  it 'Carga la página de login' do
+  visit '/login'
+  expect(200).to eq(page.status_code)
+  end
+  
+  it "Carga la página de registro" do
+    visit '/registro'
+    expect(200).to eq(page.status_code)
+  end
+end
