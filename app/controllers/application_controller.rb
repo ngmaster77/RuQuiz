05cbf38537ba34480a14c01c62e44082660842e8
Alejandro Arzola García <alu0100814712@ull.edu.es>
2017-11-23 19:26:21 +0000

    Mostrando cuestionarios realizados por el profesor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,10 +62,17 @@ class ApplicationController < Sinatra::Base
 
   get '/home' do
     @user = User.find(session[:id])
-    @resultados = Resultado.joins(:user,:cuestionario).where(resultados: {user_id: @user.id}).select("titulo,nota,notamaxima,creador,descripcion,cuestionario_id,fechacre,fechares").group("cuestionario_id").having("max(nota is not null)")
-    @resultados.each do |resultado|
-      resultado.fechacre = TimeDifference.between(resultado.fechacre, Time.now).humanize
-      resultado.fechares = TimeDifference.between(resultado.fechares, Time.now).humanize
+    if !@user.instructor
+      @resultados = Resultado.joins(:user,:cuestionario).where(resultados: {user_id: @user.id}).select("titulo,nota,notamaxima,creador,descripcion,cuestionario_id,fechacre,fechares,notaaprobar").group("cuestionario_id").having("max(nota is not null)")
+      @resultados.each do |resultado|
+        resultado.fechacre = TimeDifference.between(resultado.fechacre, Time.now).humanize
+        resultado.fechares = TimeDifference.between(resultado.fechares, Time.now).humanize
+      end
+    else
+      @resultados = Cuestionario.where(creador: @user.name)
+      @resultados.each do |resultado|
+        resultado.fechacre = TimeDifference.between(resultado.fechacre, Time.now).humanize
+      end
     end
     erb :home
   end
---
+++ var/2.rb
@@ -0,0 +1,3 @@
+quiz 'Prueba #002' do
+truefalse 'Verdadero', true
+end
