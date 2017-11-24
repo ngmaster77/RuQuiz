b778904f55f201fb113f8b3c9909baefc9a7128d
Carlos de Armas <alu0100816167@ull.edu.es>
2017-11-24 09:07:36 +0000

    Adaptando las pruebas a las nuevas vistas

--- test/app_test.rb
+++ test/app_test.rb
@@ -33,7 +33,7 @@ describe '# Prueba inicio de sesión/registro', type: :feature do
     fill_in 'password', with: '12345'
     click_button 'Log in'
     expect(200).to eq(page.status_code)
-    expect(current_path).to eql('/home_profesor')
+    expect(current_path).to eql('/home')
   end
 
   it 'Registro de usuario' do
@@ -41,10 +41,10 @@ describe '# Prueba inicio de sesión/registro', type: :feature do
     fill_in 'name', with: 'testuser'
     fill_in 'email', with: 'testuser@test.com'
     fill_in 'password', with: '12345'
-    choose('exampleRadios2') # no es profesor
+    choose('exampleRadios2')
     click_button 'Registro'
     expect(200).to eq(page.status_code)
-    expect(current_path).to eql('/home_alumno')
+    expect(current_path).to eql('/home')
     User.find_by(name: 'testuser').delete
   end
 
