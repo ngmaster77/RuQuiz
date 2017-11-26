65afd1c8ec51f909c2b5d57195942da542fa77f0
Carlos de Armas <alu0100816167@ull.edu.es>
2017-11-26 15:59:26 +0000

    reorganizando directorios en /test

--- test/acceptance/app_test.rb
+++ test/acceptance/app_test.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-require_relative 'spec_helper'
-require_relative '../config/environment'
+require_relative '../spec_helper'
+require_relative '../../config/environment'
 require 'capybara'
 require 'capybara/rspec'
 require 'capybara/dsl'
