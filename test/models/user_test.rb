40bb19b2003bcc92c4450c2656a6271e8da8e35d
Carlos de Armas <alu0100816167@ull.edu.es>
2017-11-26 15:57:10 +0000

    test modelo usuario pasa correctamente

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,28 @@
 #!/usr/bin/env ruby
 require_relative '../spec_helper'
 require_relative '../../config/environment'
-require "minitest/autorun"
+require 'minitest/spec'
+require 'minitest/autorun'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: 'Tony', email: 'maraco@negosio.com', password: '12345')
+    @user.instructor = false
+  end
+
+  test 'usuario válido' do
+    assert @user.valid?
+  end
+
+  test 'inválido sin nombre' do
+    @user.name = nil
+    refute @user.valid?, 'el usuario no es válido, se requiere el nombre'
+    assert_not_nil @user.errors[:name], 'no validation error for name present'
+  end
+
+  test 'inválido sin email' do
+    @user.email = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:email]
+  end
+end
